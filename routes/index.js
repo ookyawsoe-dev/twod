1981fd1425695ab45e92b0c9024feaa1c84f0102
ookyawsoe-dev <ookyawsoe2000@gmail.com>
2024-01-11 01:28:19 +0630

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async function (req, res, next) {
+  const filtered_date = req.query.date;
   try {
     const today = new Date();
-    // const formattedDate = today.toISOString().split('T')[0];
-    var year = today.getFullYear();
-    var month = String(today.getMonth() + 1).padStart(2, '0');
-    var day = String(today.getDate()).padStart(2, '0');
-    var formattedDate = `${year}-${month}-${day}`;
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const formattedDate = filtered_date ? filtered_date : `${year}-${month}-${day}`;
+
     const url = `https://api.thaistock2d.com/2d_result?date=${formattedDate}`;
     const response = await fetch(url);
+
     if (!response.ok) {
       throw new Error(`Failed to fetch data. Status: ${response.status}`);
     }
@@ -19,6 +21,7 @@ router.get('/', async function (req, res, next) {
 
     res.render('index', {
       title: 'MA HTET 2D',
+      query: req.query,
       data
     });
   } catch (error) {
@@ -30,6 +33,5 @@ router.get('/', async function (req, res, next) {
     });
   }
 });
-
 
 module.exports = router;
