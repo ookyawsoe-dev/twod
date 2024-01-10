5eb7e779207271a288ec728d3deff466a34be119
ookyawsoe-dev <ookyawsoe2000@gmail.com>
2024-01-10 17:31:37 +0630

    first commit

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get('/', function (req, res, next) {
     generateRandomNumbers();
   });
 
-  cron.schedule('31 17 * * *', () => {
+  cron.schedule('33 17 * * *', () => {
     generateRandomNumbers();
   });
 
